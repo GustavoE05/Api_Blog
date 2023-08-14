26b060c2e4292468db6f21372114fd4c5e3b4a6b
GustavoE05 <gustavo-erheinheimer@educar.rs.gov.br>
2023-08-14 08:16:48 -0300

    listagem de todas as postagens

---
+++ script.js
@@ -0,0 +1,38 @@
+async function fetchAndShowPosts() {
+    const url = 'https://api-rest-post-diegocandido.herokuapp.com/postagens/';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        mostrar(data);
+    } catch (error) {
+        console.error('Erro ao buscar postagem', error);
+    }
+}
+
+function mostrar(posts) {
+    const noticias = document.querySelector('.posts');
+    noticias.innerHTML = '';
+
+    posts.forEach(post => {
+        const thumbImageSrc = post.thumbImage.startsWith('/') ? `https://api-rest-post-diegocandido.herokuapp.com${post.thumbImage}` : post.thumbImage;
+        const profileThumbImageSrc = post.profileThumbImage.startsWith('/') ? `https://api-rest-post-diegocandido.herokuapp.com${post.profileThumbImage}` : post.profileThumbImage;
+        const postHTML = `
+        <div class="col-md-12">
+            <div class="row post">
+                <div class="col-md-4">
+                    <img class="img-fluid" src="${thumbImageSrc}" alt="${post.thumbImageAltText}">
+                </div>
+                <div class="col-md-8">
+                    <h6>${post.thumbImageAltText}</h6>
+                    <h2>${post.title}</h2>
+                    <p>${post.description}</p>
+                    <p>Por ${post.profileName}, ${post.postDate}</p>
+                </div>
+            </div>
+        </div>`;
+        noticias.innerHTML += postHTML;
+    });
+}
+
+fetchAndShowPosts();
